b8322ea06f6fd859fbfb6d2e716f1556faa8181e
Philip Ross <dev@baseballlover723.com>
2023-12-05 13:24:19 -0800

    day 4

---
+++ day4a.rb
@@ -0,0 +1,41 @@
+require "./base"
+
+def solve(arg)
+  cards = parse_input(arg)
+  # puts "cards: #{cards}"
+
+  sum = 0
+  cards.each do |id, card|
+    matches = card[:numbers].intersection(card[:winning])
+    # puts "#{id}: #{matches}"
+    next if matches.empty?
+    sum += 2 ** (matches.size - 1)
+  end
+  sum
+end
+
+def parse_input(input)
+  cards = {}
+  input.split("\n").each do |card_str|
+    id_str, numbs_str = card_str.split(":")
+    id = id_str[/\d+/].to_i
+    card = {winning: Set.new, numbers: Set.new}
+    winning_str, numbers_str = numbs_str.split("|")
+    winning_str.split(" ").each do |win|
+      card[:winning] << win.strip.to_i
+    end
+    numbers_str.split(" ").each do |numb|
+      card[:numbers] << numb.strip.to_i
+    end
+    cards[id] = card
+  end
+  cards
+end
+
+# test_run("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
+run(__FILE__)
