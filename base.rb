f37786c109398ef60cff713e4d1a72a1665a7484
Philip Ross <dev@baseballlover723.com>
2023-12-11 18:29:56 -0800

    only show realtime for timing

--- base.rb
+++ base.rb
@@ -5,13 +5,28 @@ def run(file_name)
   puts "solving \"#{file_name}\""
   input = File.read(file_name[0..-2] + "_input.txt").strip
   result = nil
-  time = Benchmark.measure do
+  time = Benchmark.realtime do
     result = solve(input)
   end
   puts "result"
   puts result.inspect
-  puts "                  user     system      total        real"
-  puts "time taken: #{time}"
+  puts "time taken: #{to_human_duration(time)}"
+end
+
+def to_human_duration(time)
+  time *= 1_000
+  ss, ms = time.divmod(1000)
+  mm, ss = ss.divmod(60)
+  hh, mm = mm.divmod(60)
+  dd, hh = hh.divmod(24)
+  str = ""
+  str << "#{dd} days, " if dd > 0
+  str << "#{hh} hours, " if hh > 0
+  str << "#{mm} mins, " if mm > 0
+  str << "#{ss} sec, " if ss > 0
+  str << "#{ms.round(3)} ms, " if ms > 0
+  str = str[0..-3]
+  str.reverse.sub(" ,", " and ".reverse).reverse
 end
 
 def parse_input_into_map(arg)
