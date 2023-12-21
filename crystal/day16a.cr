8984bb0fc8793dcded8de079ed0a143a8b547967
Philip Ross <dev@baseballlover723.com>
2023-12-20 22:07:24 -0800

    day 16

---
+++ crystal/day16a.cr
@@ -0,0 +1,131 @@
+require "./base"
+
+class Day16a < Base
+  DIRECTIONS = {north: 0, west: 1, south: 2, east: 3}
+
+  def solve(arg)
+    matrix = parse_input(arg)
+    # puts "matrix: "
+    # matrix.each do |row|
+    #   puts row.join
+    # end
+
+    max_y = matrix.size
+    max_x = matrix[0].size
+
+    simulate_beams(matrix, max_y, max_x, {0, 0, :east})
+  end
+
+  def simulate_beams(matrix, max_y, max_x, start)
+    bools = Array.new(max_y) { Array.new(max_x) { StaticArray(Bool, 4).new(false) } }
+    queue = [start]
+
+    while !queue.empty?
+      x, y, direction = queue.shift
+      next if x < 0 || x >= max_x || y < 0 || y >= max_y
+      bool_arr = bools[y][x]
+      next if bool_arr[DIRECTIONS[direction]]
+      bool_arr[DIRECTIONS[direction]] = true
+      bools[y][x] = bool_arr
+      # puts "index: #{DIRECTIONS[direction]}, bool_arr[DIRECTIONS[direction]]: #{bool_arr[DIRECTIONS[direction]]}"
+      case matrix[y][x]
+      when '.'
+        queue << get_next_tuple(x, y, direction)
+      when '\\'
+        queue << handle_forward_slash(x, y, direction)
+      when '/'
+        queue << handle_backward_slash(x, y, direction)
+      when '-'
+        case direction
+        when :east, :west
+          queue << get_next_tuple(x, y, direction)
+        when :north, :south
+          queue << get_next_tuple(x, y, :west)
+          queue << get_next_tuple(x, y, :east)
+        end
+      when '|'
+        case direction
+        when :north, :south
+          queue << get_next_tuple(x, y, direction)
+        when :east, :west
+          queue << get_next_tuple(x, y, :north)
+          queue << get_next_tuple(x, y, :south)
+        end
+      end
+    end
+
+    # puts "bools: "
+    # bools.each do |row|
+    #   puts row.map {|b| b ? '#' : '.'}.join
+    # end
+    energized = bools.sum do |row|
+      row.sum { |b| b.any? ? 1 : 0 }
+    end
+
+    energized
+  end
+
+  def handle_backward_slash(x, y, direction)
+    case direction
+    when :north
+      get_next_tuple(x, y, :east)
+    when :south
+      get_next_tuple(x, y, :west)
+    when :west
+      get_next_tuple(x, y, :south)
+    when :east
+      get_next_tuple(x, y, :north)
+    else
+      raise "Invalid direction"
+    end
+  end
+
+  def handle_forward_slash(x, y, direction)
+    case direction
+    when :north
+      get_next_tuple(x, y, :west)
+    when :south
+      get_next_tuple(x, y, :east)
+    when :west
+      get_next_tuple(x, y, :north)
+    when :east
+      get_next_tuple(x, y, :south)
+    else
+      raise "Invalid direction"
+    end
+  end
+
+  def get_next_tuple(x, y, direction)
+    case direction
+    when :north
+      {x, y - 1, direction}
+    when :south
+      {x, y + 1, direction}
+    when :west
+      {x - 1, y, direction}
+    when :east
+      {x + 1, y, direction}
+    else
+      raise "Invalid direction"
+    end
+  end
+
+  def parse_input(input)
+    input.split('\n').map do |str|
+      str.chars
+    end
+  end
+end
+
+stop_if_not_script(__FILE__)
+# test_run(".|...\\....
+# |.-.\\.....
+# .....|-...
+# ........|.
+# ..........
+# .........\\
+# ..../.\\\\..
+# .-.-/..|..
+# .|....-|.\\
+# ..//.|....")
+run(__FILE__)
---
+++ day16e.rb
@@ -0,0 +1,192 @@
+require "parallel"
+require "./base"
+
+# https://www.reddit.com/r/adventofcode/comments/18jjpfk/2023_day_16_solutions/kdmkw4f/
+# parallelized
+def solve(arg)
+  matrix = parse_input(arg)
+
+  max_mirror_energy(matrix)
+end
+
+class Traversal
+  attr_accessor :layout, :rows, :cols
+
+  UP = 1 unless defined? UP
+  RIGHT = 2 unless defined? RIGHT
+  DOWN = 4 unless defined? DOWN
+  LEFT = 8 unless defined? LEFT
+
+  SPLITTER_HORIZ = 0 unless defined? SPLITTER_HORIZ
+  SPLITTER_VERT = 1 unless defined? SPLITTER_VERT
+  MIRROR_FWD = 2 unless defined? MIRROR_FWD
+  MIRROR_BWD = 3 unless defined? MIRROR_BWD
+  OPEN = 4 unless defined? OPEN
+
+  def initialize(layout, rows, cols)
+    @layout = layout
+    @rows = rows
+    @cols = cols
+  end
+
+  def clone
+    Traversal.new(@layout.clone, @rows, @cols)
+  end
+
+  def energized
+    traverse(0, 0, RIGHT)
+  end
+
+  def idx(row, col)
+    row * @cols + col
+  end
+
+  def step(row, col, dir)
+    case dir
+    when UP
+      return nil if row.zero?
+      [row - 1, col]
+    when RIGHT
+      return nil if col == @cols - 1
+      [row, col + 1]
+    when DOWN
+      return nil if row == @rows - 1
+      [row + 1, col]
+    when LEFT
+      return nil if col.zero?
+      [row, col - 1]
+    else
+      raise 'Invalid direction'
+    end
+  end
+
+  def traverse(row, col, dir)
+    energized = 0
+    # puts "row: #{row}, col: #{col}, dir: #{dir}"
+
+    loop do
+      idx = idx(row, col)
+      curr = @layout[idx] & 0xF
+      visited = @layout[idx] >> 4
+
+      break if (dir & visited) != 0
+
+      if visited.zero?
+        energized += 1
+      end
+
+      @layout[idx] |= dir << 4
+
+      case curr
+      when Traversal::SPLITTER_HORIZ
+        if dir == Traversal::UP || dir == Traversal::DOWN
+          step_r = step(row, col, Traversal::LEFT)
+          if (step_r)
+            r, c = step_r
+            energized += traverse(r, c, Traversal::LEFT)
+          end
+          dir = Traversal::RIGHT
+        end
+      when Traversal::SPLITTER_VERT
+        if dir == Traversal::LEFT || dir == Traversal::RIGHT
+          step_r = step(row, col, Traversal::UP)
+          if (step_r)
+            r, c = step_r
+            energized += traverse(r, c, Traversal::UP)
+          end
+          dir = Traversal::DOWN
+        end
+      when Traversal::MIRROR_FWD
+        case dir
+        when Traversal::UP then dir = Traversal::RIGHT
+        when Traversal::RIGHT then dir = Traversal::UP
+        when Traversal::DOWN then dir = Traversal::LEFT
+        when Traversal::LEFT then dir = Traversal::DOWN
+        else
+          raise 'Invalid dir'
+        end
+      when Traversal::MIRROR_BWD
+        case dir
+        when Traversal::UP then dir = Traversal::LEFT
+        when Traversal::RIGHT then dir = Traversal::DOWN
+        when Traversal::DOWN then dir = Traversal::RIGHT
+        when Traversal::LEFT then dir = Traversal::UP
+        else
+          raise 'Invalid dir'
+        end
+      else
+      end
+
+      step_r = step(row, col, dir)
+      if (step_r)
+        r, c = step_r
+        row = r
+        col = c
+      else
+        break
+      end
+    end
+
+    energized
+  end
+end
+
+def mirror_energy(input)
+  input.clone.energized
+end
+
+def max_mirror_energy(input)
+  # puts "input.class: #{input.class}"
+  traversals = []
+  (0...input.rows).each do |r|
+    traversals.push([r, 0, Traversal::RIGHT])
+    traversals.push([r, input.cols - 1, Traversal::LEFT])
+  end
+  (0...input.cols).each do |c|
+    traversals.push([0, c, Traversal::DOWN])
+    traversals.push([input.rows - 1, c, Traversal::UP])
+  end
+  # puts "traversals.size: #{traversals.size}"
+
+  Parallel.map(traversals) { |(r, c, d)| input.clone.traverse(r, c, d) }
+    .max
+end
+
+def parse_input(input)
+  cols = input.index("\n")
+  rows = input.length / cols
+  layout = input.chars
+             .reject { |b| b == "\n" } # Filter out newlines
+             .map do |b|
+    case b
+    when '-' then Traversal::SPLITTER_HORIZ
+    when '|' then Traversal::SPLITTER_VERT
+    when '/' then Traversal::MIRROR_FWD
+    when '\\' then Traversal::MIRROR_BWD
+    when '.' then Traversal::OPEN
+    else
+      raise 'Invalid elem'
+    end
+  end
+
+  # puts "layout: #{layout}"
+
+  Traversal.new(layout, rows, cols)
+
+  # input.split("\n").map do |str|
+  #   str.chars
+  # end
+end
+
+return if __FILE__ != $0
+# test_run(".|...\\....
+# |.-.\\.....
+# .....|-...
+# ........|.
+# ..........
+# .........\\
+# ..../.\\\\..
+# .-.-/..|..
+# .|....-|.\\
+# ..//.|....")
+run(__FILE__)
